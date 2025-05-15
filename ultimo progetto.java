a226505be590a5479b3f01e3b6e475e29a13c56c
Dale1234m <mattedale14@gmail.com>
2025-05-15 10:47:31 +0200

    Add files via upload

---
+++ ultimo progetto.java
@@ -0,0 +1,138 @@
+public class NodoAlberoBinario {
+
+    private Tipo info;
+    private NodoAlberoBinario psx;
+    private NodoAlberoBinario pdx;
+
+    public NodoAlberoBinario (Tipo info){
+        this.info = new Tipo(info);
+        psx = null;
+        pdx = null;
+    }
+
+    public void setInfo(Tipo info){
+        this.info = new Tipo(info);
+    }
+
+    public Tipo getInfo(){
+        return new Tipo(info);
+    }
+
+    public void setLeftChild(NodoAlberoBinario psx){
+        this.psx = psx;
+    }
+
+    public NodoAlberoBinario getLeftChild {
+        return psx;
+    } 
+
+    public void setRightChild (NodoAlberoBinario pdx) {
+        this.pdx = pdx;
+    }
+
+    public NodoAlberoBinario getRightChild(){
+        return pdx;
+    }
+}
+
+public class AlberoBinario{
+    private NodoAlberoBinario ptr;
+    
+    public AlberoBinario(){
+        ptr = null;   
+    }
+
+    public void setRadice (NodoAlberoBinario Nodo){
+        ptr = nodo;
+    }
+
+    public NodoAlberoBinario getRadice(){
+         return ptr;
+    }
+
+    private void visitaAnticipata(NodoAlberoBinario p){
+        if (p==null)
+        return ;
+        esamina (p.getInfo());
+        if (p.getLeftChild() !=null)
+        visitaAnticipata (p.getLeftChild ());
+        if (p.getRightChild());
+        visitaAnticipata (p.getRightChild ());
+        public void visitaAnticipata() {
+            visitaAnticipata(ptr);
+        }
+
+        private void visitaDifferita (NodoAlberoBinario p){
+            if (p==null)
+            return;
+            if (p.getLeftChild() !=null)
+            visitaAnticipata (p.getLeftChild ());
+            if (p.getRightChild());
+            visitaAnticipata (p.getRightChild ());
+              esamina (p.getInfo());
+                
+        }
+
+        public void visitaDifferita(){
+            visitaDifferita (ptr);
+        }
+
+        private void visitaSimmetrica(NodoAlberoBinario p){
+            if (p==null)
+            return;
+            if (p.getLeftChild() !=null)
+            visitaSimmetrica (p.getLeftChild());
+            esamina (p.getInfo());
+            if (p.getRightChild() !=null)
+            visitaSimmetrica (p.getRightChild());
+            
+        }
+
+        public void visitaSimmetrica(){
+            visitaSimmetrica(ptr);
+        }
+
+        public class nodoalberobinario {
+            private char inf;,
+            private NodoAlberoBinazio psx;
+            private NodoAlberoBinario pdx;
+            public NodoAlberoBinarto (chat info) {
+            this.info=Info:
+            psx = null;
+            pdx = null;
+            }
+            public void setinfo (char info) {
+            this info = info;
+            }
+            public char getinfo() {
+            return info;
+            }
+            public void setleftchild (NodoAlberoBinario psx) {
+            this.psx = psx;
+            }
+            public NodoAlberoBinario getLeftChild() {
+            return psx;
+            }
+            public void setRightChild (NodoAlberoBinario pdx) {
+            this.pdx = pdx:
+            }
+            publlc NodoAlberoBinario getRightchild () {
+            return pdx;
+            }
+            }
+            aggiungiNodo (n, p.getLeftChild());
+            else {// n.getInfo() >= p.getInfo()
+            if (p.getRightChild() == null)
+            p.setRightChild (n) ;
+            else 
+              aggiungiNodo (n, p.getRightChild());
+               }
+            }
+            public void aggiungiNodo (char info) {
+            NodoAlberoBinario nodo = new NodoAlberoBinario (info);
+            if (ptr == null)
+            ptr = new NodoAlberoBinario(info);
+            else
+             aggiungiNodo (nodo, ptr) ;
+            }
+    }
